313cbe9b66718c65151c128a02bc24328a60f3a5
clearmedium <azofchak@kent.edu>
2024-02-29 18:30:06 -0500

    rebase fucntions done. implementing testing

--- rebase exercise.py
+++ rebase exercise.py
@@ -1,3 +1,15 @@
+def check_errors(input_base, digits, output_base):
+
+    if input_base < 2:
+        raise ValueError("input base must be >= 2")
+    elif output_base < 2:
+        raise ValueError("output base must be >= 2")
+    
+    for digit in digits:
+        if digit < 0 or digit >= input_base:
+            raise ValueError("all digits must satisfy 0 <= d < input base")
+
+
 def convert_to_10(input_base, digits):
     base_10_result = 0
 
@@ -8,36 +20,12 @@ def convert_to_10(input_base, digits):
 
 def rebase(input_base, digits, output_base):
     base_10_number = convert_to_10(input_base, digits)
-    remainders = 0
+    remainders = []
 
-    while base_10_number >= 1:
-        base_10_number /= output_base
-        remainders += base_10_number / output_base
-        output_digits = ''.join([str(r) if r < 10 else chr(r - 10 + ord('A')) for r in reversed(remainders)])
-
-    return output_digits
-
-
-
-
-
-
-
-
-
-
-
-
-
+    while base_10_number != 0:
+        remainders.append(base_10_number % output_base)
+        base_10_number = base_10_number // output_base
     
-# The number 42, in base 10, means:
+    return remainders[::-1]
 
-# (4 * 10^1) + (2 * 10^0)
 
-# The number 101010, in base 2, means:
-
-# (1 * 2^5) + (0 * 2^4) + (1 * 2^3) + (0 * 2^2) + (1 * 2^1) + (0 * 2^0)
-
-# The number 1120, in base 3, means:
-
-# (1 * 3^3) + (1 * 3^2) + (2 * 3^1) + (0 * 3^0)
